2942fe5bb4e243f3c6392dca7eb2e4833620600c
Mihran <mihranmamyan@gmail.com>
2018-11-17 22:19:33 +0400

    JAVASWEEPER

--- src/JavaSweeper.java
+++ src/JavaSweeper.java
@@ -1,12 +1,36 @@
 import javax.swing.*;
+import java.awt.*;
+import sweeper.Box;
 
 public class JavaSweeper extends JFrame {
+    private JPanel panel;
+    private final int COLS = 15;
+    private final int ROWS = 1;
+    private final int IMAGE_SIZE = 50;
+
     public static void main(String[] args) {
         new JavaSweeper();
 
     }
 private JavaSweeper(){
+        setImages();
+        initPanel();
         initFrame();
+
+}
+private void initPanel(){
+        panel = new JPanel(){
+            @Override
+            protected void paintComponent(Graphics g) {
+                super.paintComponent(g);
+                for (Box box: Box.values())
+               g.drawImage((Image)box.image, box.ordinal()*IMAGE_SIZE, 0,this);
+
+
+            }
+        };
+        panel.setPreferredSize(new Dimension(COLS*IMAGE_SIZE,ROWS*IMAGE_SIZE));
+        add (panel);
 
 }
 
@@ -17,5 +41,15 @@ private void initFrame (){
         setLocationRelativeTo(null);
         setResizable(false);
         setVisible(true);
+}
+private void setImages() {
+        for(Box box:Box.values())
+            box.image = getImage(box.name());
+}
+private Image getImage (String name){
+        String filename = "img/" + name.toLowerCase() + ".png";
+        ImageIcon icon = new ImageIcon(getClass().getResource(filename));
+        return icon.getImage();
+
 }
 }
